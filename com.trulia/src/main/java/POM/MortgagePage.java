a9fc898b8644178414f27570989d5e1ffef438f4
Osman <mdaziz07@gmail.com>
2021-10-07 19:09:32 -0400

    add 6 more test cases for Trulia MortgagePage

--- com.trulia/src/main/java/POM/MortgagePage.java
+++ com.trulia/src/main/java/POM/MortgagePage.java
@@ -40,7 +40,50 @@ public class MortgagePage extends Base {
     @FindBy(xpath = "//*[@id=\"main-content\"]//div[contains(text(), 'Looking for a')]")
     WebElement contentForMortgageOverviewPage;
 
+    @FindBy(xpath = "//*[@id=\"long-form\"]/div//h1[contains(text(), 'Talk')]")
+    WebElement contentForPreQualifiedPage;
+
+    @FindBy(xpath = "//*[@id=\"main-content\"]/div[1]//h1/div")
+    WebElement contentForMortgageRatePage;
+
+    @FindBy(xpath = "//*[@id=\"main-content\"]//div[2]//h1/div")
+    WebElement contentForRefinanceCalculatorPage;
+
+    @FindBy(xpath = "//*[@id=\"main-content\"]//div[2]//h1/div")
+    WebElement contentForMortgageCalculatorPage;
+
+    @FindBy(xpath = "//*[@id=\"__next\"]/div[2]/div/main//h1/div")
+    WebElement contentForAffordabilityCalculatorPage;
+
+    @FindBy(xpath = "//*[@id=\"main-content\"]/div[1]//h1/div")
+    WebElement contentForRefinanceRatesPage;
+
+    public boolean isContentForRefinanceRatesPage(){ return isElementPresent(contentForRefinanceRatesPage);}
+
+    public boolean isContentForAffordabilityCalculatorPage(){return isElementPresent(contentForAffordabilityCalculatorPage);}
+
+    public boolean isContentForMortgageCalculatorPagePresent(){return isElementPresent(contentForMortgageCalculatorPage);}
+
+    public boolean isContentForRefinanceCalculatorPagePresent(){return isElementPresent(contentForRefinanceCalculatorPage);}
+
+    public boolean isContentForMortgageRatePagePresent(){return isElementPresent(contentForMortgageRatePage);}
+
+    public boolean isContentForPreQualifiedPagePresent(){return isElementPresent(contentForPreQualifiedPage);}
+
     public boolean isContentForMortgageOverviewPagePresent(){return isElementPresent(contentForMortgageOverviewPage);}
+
+
+    void clickOnSubMenuRefinanceCalculator(){clickOnElement(subMenuRefinanceCalculator);}
+
+    void clickOnSubMenuAffordabilityCalculator(){clickOnElement(subMenuAffordabilityCalculator);}
+
+    void clickOnsSubMenuMortgageCalculator(){clickOnElement(subMenuMortgageCalculator);}
+
+    void clickOnSubMenuRefinanceRates(){clickOnElement(subMenuRefinanceRates);}
+
+    void clickOnSubMenuMortgageRates(){clickOnElement(subMenuMortgageRates);}
+
+    void clickOnSubMenuGetPreQualified(){clickOnElement(subMenuGetPreQualified);}
 
     void clickOnSubMenuMortgageOverview(){clickOnElement(subMenuMortgageOverview);}
 
@@ -48,6 +91,43 @@ public class MortgagePage extends Base {
         return getStringListFromADiv(By.xpath("//*[@id=\"__next\"]//header//li[3]//ul//div"));}
 
     void mouseHoverOnMortgageNavMenu(){mouseHoverOnAnElement(mortgageNavMenu);}
+
+
+    public void doNavigateToRefinanceCalculatorPage(){
+        visibilityOfMortgageNavDropdownMenu();
+        clickOnSubMenuRefinanceCalculator();
+        waitForElementToBeVisible(contentForRefinanceCalculatorPage);
+    }
+
+    public void doNavigateToAffordabilityCalculatorPage(){
+        visibilityOfMortgageNavDropdownMenu();
+        clickOnSubMenuAffordabilityCalculator();
+        waitForElementToBeVisible(contentForAffordabilityCalculatorPage);
+    }
+
+    public void doNavigateToMortgageCalculatorPage(){
+        visibilityOfMortgageNavDropdownMenu();
+        clickOnsSubMenuMortgageCalculator();
+        waitForElementToBeVisible(contentForRefinanceCalculatorPage);
+    }
+
+    public void doNavigateToRefinanceRatesPage(){
+        visibilityOfMortgageNavDropdownMenu();
+        clickOnSubMenuRefinanceRates();
+        waitForElementToBeVisible(contentForRefinanceRatesPage);
+    }
+
+    public void doNavigateToMortgageRatesPage(){
+        visibilityOfMortgageNavDropdownMenu();
+        clickOnSubMenuMortgageRates();
+        waitForElementToBeVisible(contentForMortgageRatePage);
+    }
+
+    public void doNavigateToGetPreQualifiedPage(){
+        visibilityOfMortgageNavDropdownMenu();
+        clickOnSubMenuGetPreQualified();
+        waitForElementToBeVisible(contentForPreQualifiedPage);
+    }
 
     public void doNavigateToMortgageOverviewPage(){
         visibilityOfMortgageNavDropdownMenu();
--- com.trulia/src/test/java/testPOM/TestMortgagePage.java
+++ com.trulia/src/test/java/testPOM/TestMortgagePage.java
@@ -25,12 +25,66 @@ public class TestMortgagePage extends TestBase {
 
     }
 
-    @Test(description = "ID:02", priority = 2, enabled = true)
+    @Test(description = "ID:02", priority = 2, enabled = false)
     public void testDoNavigateToMortgageOverviewPage() throws IOException {
 
         mortgagePage = getMortgagePage();
         mortgagePage.doNavigateToMortgageOverviewPage();
         Assert.assertTrue(mortgagePage.isContentForMortgageOverviewPagePresent());
+
+    }
+
+    @Test(description = "ID:03", priority = 3, enabled = false)
+    public void testDoNavigateToGetPreQualifiedPage() throws IOException {
+
+        mortgagePage = getMortgagePage();
+        mortgagePage.doNavigateToGetPreQualifiedPage();
+        Assert.assertTrue(mortgagePage.isContentForPreQualifiedPagePresent());
+
+    }
+
+    @Test(description = "ID:04", priority = 4, enabled = false)
+    public void testDoNavigateToMortgageRatesPage() throws IOException {
+
+        mortgagePage = getMortgagePage();
+        mortgagePage.doNavigateToMortgageRatesPage();
+        Assert.assertTrue(mortgagePage.isContentForMortgageRatePagePresent());
+
+    }
+
+    @Test(description = "ID:05", priority = 5, enabled = false)
+    public void testDoNavigateToRefinanceRatesPage() throws IOException {
+
+        mortgagePage = getMortgagePage();
+        mortgagePage.doNavigateToRefinanceRatesPage();
+        Assert.assertTrue(mortgagePage.isContentForRefinanceRatesPage());
+
+    }
+
+    @Test(description = "ID:06", priority = 6, enabled = false)
+    public void testDoNavigateToMortgageCalculatorPage() throws IOException {
+
+        mortgagePage = getMortgagePage();
+        mortgagePage.doNavigateToMortgageCalculatorPage();
+        Assert.assertTrue(mortgagePage.isContentForMortgageCalculatorPagePresent());
+
+    }
+
+    @Test(description = "ID:07", priority = 7, enabled = false)
+    public void testDoNavigateToAffordabilityCalculatorPage() throws IOException {
+
+        mortgagePage = getMortgagePage();
+        mortgagePage.doNavigateToAffordabilityCalculatorPage();
+        Assert.assertTrue(mortgagePage.isContentForAffordabilityCalculatorPage());
+
+    }
+
+    @Test(description = "ID:08", priority = 8, enabled = false)
+    public void testDoNavigateToRefinanceCalculatorPage() throws IOException {
+
+        mortgagePage = getMortgagePage();
+        mortgagePage.doNavigateToRefinanceCalculatorPage();
+        Assert.assertTrue(mortgagePage.isContentForRefinanceCalculatorPagePresent());
 
     }
 }
